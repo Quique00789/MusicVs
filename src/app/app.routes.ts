f33596cefaf887e120e37efdc7f8630ab015dfc5
Leonardo Trejo <123757814+Quique00789@users.noreply.github.com>
2025-10-23 09:06:23 -0600

    Revert: simplify routes back to Home and Auth only
    - Remove lazy routes for pages (discover, playlists, artists, favorites)
    - Restore minimal routing to the stable state

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,29 +5,5 @@ import { HomeComponent } from './home.component';
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'auth', loadComponent: () => AuthComponent },
-  { 
-    path: 'discover', 
-    loadComponent: () => import('./pages/discover.component.ts').then(m => m.DiscoverComponent)
-  },
-  { 
-    path: 'playlists', 
-    loadComponent: () => import('./pages/playlists.component.ts').then(m => m.PlaylistsComponent)
-  },
-  { 
-    path: 'playlist/:id', 
-    loadComponent: () => import('./pages/playlist-detail.component.ts').then(m => m.PlaylistDetailComponent)
-  },
-  { 
-    path: 'artists', 
-    loadComponent: () => import('./pages/artists.component.ts').then(m => m.ArtistsComponent)
-  },
-  { 
-    path: 'artist/:id', 
-    loadComponent: () => import('./pages/artist-detail.component.ts').then(m => m.ArtistDetailComponent)
-  },
-  { 
-    path: 'favorites', 
-    loadComponent: () => import('./pages/favorites.component.ts').then(m => m.FavoritesComponent)
-  },
   { path: '**', redirectTo: '' }
 ];

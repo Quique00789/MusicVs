50751fa21d37159fc5f33b201ebafb1e86dd7124
MiguelV467 <156325594+MiguelV467@users.noreply.github.com>
2025-10-23 08:58:22 -0700

    Agregar rutas para artistas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import { AuthComponent } from './auth.component';
 import { HomeComponent } from './home.component';
+import { ArtistsComponent } from './artists.component';
+import { ArtistDetailComponent } from './artist-detail.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },  // Ruta raíz
   { path: 'auth', loadComponent: () => AuthComponent },
+  { path: 'artists', component: ArtistsComponent },  // Nueva ruta para artistas
+  { path: 'artist/:id', component: ArtistDetailComponent },  // Nueva ruta para detalle de artista
   { path: '**', redirectTo: '' }  // Redirección para rutas no encontradas
 ];
